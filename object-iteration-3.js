4a66a23f7830c1bcd93e8c1ecd0f2d33932b15ea
Travis Hubbard <t3patterson@gmail.com>
2017-02-09 06:51:36 -0500

    object iteration 3 files

---
+++ object-iteration-3.js
@@ -0,0 +1,20 @@
+// ---------------------------------------
+// EXERCISE : countTheHoles()
+// ---------------------------------------
+// Write a `countTheHoles` function that takes an intenteger and counts the 
+// number of holes for each number.
+//
+// For example: the number 8042 would have 4 holes:
+//  - 2 holes in the 8
+//  - 1 hole in the 0
+//  - 1 hole in the 4
+
+var countTheHoles = function(){}
+
+
+//~+~+~+~+~+~+~+~Don't Touch~+~+~++~+~+~+~+~+~+~+
+console.assert(  countTheHoles(301923) === 2  )
+console.assert(  countTheHoles(880216) === 6  )
+console.assert(  countTheHoles(40442) === 4 )
+console.assert(  countTheHoles(808) === 5  )
+
